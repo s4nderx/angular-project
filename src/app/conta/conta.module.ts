f65aea34771dac3d792df2a58bb2fbaab22b3c7d
s4nderx <phellipe.sander1@gmail.com>
2021-02-23 23:11:46 -0300

    configurando modulo de conta (cadastro e login) com lazzy loading

---
+++ src/app/conta/conta.module.ts
@@ -0,0 +1,31 @@
+/* Angular modules */
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import {HttpClientModule} from '@angular/common/http'
+
+/* My Components */
+import { CadastroComponent } from './cadastro/cadastro.component';
+import { LoginComponent } from './login/login.component';
+
+/* My Modules */
+import { ContaRouterModule } from './conta.route';
+import { ContaAppComponent } from './conta.app.component';
+
+@NgModule({
+  declarations: [
+    ContaAppComponent,
+    CadastroComponent,
+    LoginComponent
+  ],
+  imports: [
+    CommonModule,
+    RouterModule,
+    ContaRouterModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule
+  ]
+})
+export class ContaModule { }
